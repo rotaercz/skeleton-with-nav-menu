6bd91d11b153cc32eab7afb99cee29e7ee07b488
Howard Kim <rotaercz@gmail.com>
2017-01-10 17:12:13 -0800

    Add files via upload

---
+++ js/gh.js
@@ -0,0 +1,53 @@
+// toggle mobile navigation
+function toggleNav() {
+	if( $("#nav").hasClass("mobile") ) {
+		$("#nav").removeClass("mobile");
+	}
+	else {
+		$("#nav").addClass("mobile");
+	}
+}
+
+// hide mobile nav when clicking outside of nav area
+$("html").click(function() {
+	$("#nav").removeClass("mobile");
+});
+
+$("#nav").click(function(e){
+    e.stopPropagation();
+});
+
+// hide nav on scroll down
+var scrolled = false;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $("#nav").outerHeight();
+
+$(window).scroll(function(event){
+    scrolled = true;
+});
+
+setInterval(function() {
+    if (scrolled) {
+        hasScrolled();
+        scrolled = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    if(Math.abs(lastScrollTop - st) <= delta) // make sure user scrolled more than delta
+        return;
+    
+    if (st > lastScrollTop && st > navbarHeight) {
+		$("#nav").removeClass("mobile").removeClass("nav-show").addClass("nav-hide");
+    } 
+	else {
+        if(st + $(window).height() < $(document).height()) {
+            $("#nav").removeClass("nav-hide").addClass("nav-show");
+        }
+    }
+    
+    lastScrollTop = st;
+}
